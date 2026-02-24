336317117e676134d08fed00e7f88366013c7de3
Aman <amanjha434@gmail.com>
2026-02-25 00:33:36 +0530

    Working server with 3 workers for task processing

--- v0_concurrency.py
+++ v0_concurrency.py
@@ -1,9 +1,11 @@
 import threading
 import queue
-import random
 import time
+from fastapi import FastAPI
+import uuid
+import uvicorn
 
-
+app = FastAPI()
 
 job_queue = queue.Queue()
 results = {}
@@ -15,6 +17,7 @@ def worker(worker_id ):
 
     while True:
         job_id , job_duration = job_queue.get()
+
         print(f"Worker {worker_id} is processing a job for {job_duration} seconds.")
         with results_loc:
             results[job_id] = "Processing"
@@ -28,20 +31,35 @@ def worker(worker_id ):
         job_queue.task_done()
 
 
-for i in range(3):
-    t = threading.Thread(target=worker, args=(i,),daemon=True)
-    t.start()
+@app.on_event("startup")
+def startup_event():
+    for i in range(3):
+        t = threading.Thread(target=worker,args=(i,),daemon=True)
+        t.start()
 
-durations = [2, 5, 1, 4, 3, 2, 1, 2, 4, 2]
-
-for i,duration in enumerate(durations):
-    print(f" Producer adding a {i}s job to the queue.")
-    job_queue.put((i,duration))
+    print("3 Workers are on standby")
 
 
-print("Main thread: Waiting for all jobs to finish...")
-job_queue.join() 
-print("All jobs are done!")
+@app.post("/submit")
+def submit(duration:int):
+    job_id = str(uuid.uuid4())
+
+    with results_loc:
+        results[job_id] = "Pending"
+
+    job_queue.put((job_id,duration))
+
+    return {"job_id":job_id,"status":"pending"}
 
 
-print(results)
+@app.get("/status/{job_id}")
+def get_status(job_id: str):
+
+    with results_loc:
+        status = results.get(job_id, "Not Found")
+    return {"job_id": job_id, "status": status}
+
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
