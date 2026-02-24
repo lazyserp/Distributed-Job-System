53e74f266de3b68d78b1815f3c79ca5206ced84c
Aman <amanjha434@gmail.com>
2026-02-24 16:49:15 +0530

    feat: Added Job status via dictionary

--- v0_concurrency.py
+++ v0_concurrency.py
@@ -6,17 +6,25 @@ import time
 
 
 job_queue = queue.Queue()
+results = {}
+results_loc = threading.Lock()
 
-def worker(worker_id : int):
+
+def worker(worker_id ):
     print("Worker has started and waiting for jobs...")
 
     while True:
-        job_duration = job_queue.get()
+        job_id , job_duration = job_queue.get()
         print(f"Worker {worker_id} is processing a job for {job_duration} seconds.")
+        with results_loc:
+            results[job_id] = "Processing"
 
         time.sleep(job_duration)
 
-        print(f"Worker {worker_id} has finished the job.")
+        with results_loc:
+            results[job_id] = "Completed"
+
+        print(f"Worker {worker_id} has finished the job {job_id}")
         job_queue.task_done()
 
 
@@ -26,9 +34,9 @@ for i in range(3):
 
 durations = [2, 5, 1, 4, 3, 2, 1, 2, 4, 2]
 
-for i in durations:
+for i,duration in enumerate(durations):
     print(f" Producer adding a {i}s job to the queue.")
-    job_queue.put(i)
+    job_queue.put((i,duration))
 
 
 print("Main thread: Waiting for all jobs to finish...")
@@ -36,3 +44,4 @@ job_queue.join()
 print("All jobs are done!")
 
 
+print(results)
